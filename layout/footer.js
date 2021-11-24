7ddea699f87be7900b2596f3bb1623b008a5783f
Fadhel ijlal falah <fadhelijlalfalah@gmail.com>
2021-11-25 00:58:18 +0700

    automatic deploy github pages

--- layout/footer.js
+++ layout/footer.js
@@ -1,5 +1,6 @@
 
 import styles from "../styles/Home.module.css"
+import Image from "next/image";
 export default function Footer(){
     return(
         <>
@@ -12,7 +13,7 @@ export default function Footer(){
             Fadhel Ijlal Falah
           </a>. Powered by
             <span className={styles.logo}>
-              <img
+              <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
                 width={72}
